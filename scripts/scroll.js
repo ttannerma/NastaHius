561ffc8806e24ef3dade79ade5de86256f027906
Teemu Koskinen <koskinen6@gmail.com>
2018-11-29 11:49:11 +0200

    Added scroll effect

---
+++ scripts/scroll.js
@@ -0,0 +1,9 @@
+let link = document.getElementById('contact').addEventListener('click', scroll)
+
+function scroll () {
+    window.scrollTo({
+        top: 5000,
+        left: 0,
+        behavior: 'smooth'
+    })
+}
