6bf7cb83bfb4adf9871c6725da2190d3d19888e2
Teemu <teemu.tannerma@cs.tamk.fi>
2018-12-03 21:23:25 +0200

    Scroll js fixed for IE and EDGE users

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,9 +1,13 @@
 let link = document.getElementById('contact').addEventListener('click', scroll)
 
 function scroll () {
-    window.scrollTo({
-        top: 5000,
-        left: 0,
-        behavior: 'smooth'
-    })
+	if (document.documentMode || /Edge/.test(navigator.userAgent)) {
+		window.location.href = "contact.html";
+	} else {
+		window.scrollTo({
+			top: 5000,
+			left: 0,
+			behavior: 'smooth'
+		})
+	}
 }
