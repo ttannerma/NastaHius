38295b168887e52fe344986a27fbac9141d127f0
Koskte <Koskinen6@gmail.com>
2018-12-09 12:14:32 +0200

    fixing

--- ajanvaraus/reservation1.2/main.js
+++ ajanvaraus/reservation1.2/main.js
@@ -15,18 +15,14 @@ function checkLength (e) {
 
 
 window.addEventListener('load', () => {
-    let box = document.getElementsByClassName('reservationConfirm') //vahvistuslomake
-    let date = document.getElementById('date')//vahvistuslomakkeessa lukeva päivä
-    let time = document.getElementById('time')//vahvistuslomakkeessa lukeva aika
-    let times = document.querySelectorAll('li')//kaikki li-elementit (ajat)
+    let box = document.getElementsByClassName('reservationConfirm')
+    let date = document.getElementById('date')
+    let time = document.getElementById('time')
+    let times = document.querySelectorAll('li')
 
     for (let i = 0; i < times.length; i++) {
-        /*
-        kaikille li-elementeille eventlistener. Tuo if-lause on tuossa sitä varten, ettei se laittaisi jo varatuille ajoille
-        eventlisteneriä. Idea siinä on se että jos aika on varattu niin sitä ei voi klikata. En siis tiedä toimiiko tuo vielä
-        koska wamp ei päivitä sitä filuu jostain syystä. (voi ottaa pois jos se jotain perseilee (sen if-lauseen siis))
-        */
-        if(times[i].style.backgroundColor != 'red') {
+
+        
 
             times[i].addEventListener('mouseup', () => {
                 box[0].style.display = 'initial'
@@ -42,7 +38,7 @@ window.addEventListener('load', () => {
                     date.value = times[i].id
                 }
             })
-        }
+        
     }
 
     function checkDay (day) {
@@ -62,17 +58,12 @@ window.addEventListener('load', () => {
     
     
     
-    document.getElementById('cancel').addEventListener('mouseup', () => { //Tää on ruksi, joka on siin varauslomakkeessa
+    document.getElementById('cancel').addEventListener('mouseup', () => {
         box[0].style.display = 'none'
         date.innerHTML = ""
         time.innerHTML = ""
     })
-    /*
-    Tää tässä pyörähtää kun sivu on käytännössä ladannu.
-    Se lähettää get-pyynnön getData.php:lle ja se php vaan hakee
-    datan mitä on tallennettuna tietokantaan. Se sitten muuttaa kaikki li-elementit
-    punasiks mitkä matchaa tietokannasta tulleen tiedon kanssa.
-    */
+
     $(document).ready(function () { 
         $.get('getData.php', function (data) {
             data = JSON.parse(data)
@@ -90,12 +81,7 @@ window.addEventListener('load', () => {
             }
         })
     })
-    /*
-    Tää on käytännössä sama ku ylempi mut tää pyörähtää kun painetaan
-    varauslomakkeessa "vahvista"-nappia. Ja sitten tää myös lähettää lomakkeessa olleet
-    tiedot handler.php:lle, joka sitten tallettaa ne tietokantaan ja samalla antaa ne datat takasin
-    sieltä ja tässä myös värjätään jos matchaa datat li-elementtien kanssa.
-    */
+
     $('button').click(function () {
         let date = $('#date').text()
         let time = $('#time').text()
@@ -117,6 +103,7 @@ window.addEventListener('load', () => {
                 },
                 success: function (data) {
                     data = JSON.parse(data)
+                    console.log(data)
                     for(let i =0; i < times.length; i++) {
                         for(let j = 0; j < data.length; j++) {
                             if(data[j].dayID == times[i].id && data[j].strTime == times[i].innerHTML) {
